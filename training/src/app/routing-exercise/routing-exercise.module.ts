8583a358e32e4a075366df5394392b168ba15932
Richard Westwater <richard.westwater@sap.com>
2020-06-16 10:17:02 +0100

    Add alias example

--- training/src/app/routing-exercise/routing-exercise.module.ts
+++ training/src/app/routing-exercise/routing-exercise.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactExerciseModule } from '../contact-exercise/contact-exercise.module';
 import { RouterModule } from '@angular/router';
-import { CmsPageGuard } from '@spartacus/storefront';
+import { CmsPageGuard, PageLayoutComponent } from '@spartacus/storefront';
 import { ContactExerciseComponent } from '../contact-exercise/contact-exercise.component';
 import { RoutingConfig, ConfigModule, PRODUCT_NORMALIZER } from '@spartacus/core';
 import { ProductPrettyNameNormalizer } from './product-name-normalizer';
@@ -16,6 +16,8 @@ import { ProductPrettyNameNormalizer } from './product-name-normalizer';
     ContactExerciseModule,
     RouterModule.forChild([
       { path: 'contact', canActivate: [CmsPageGuard], component: ContactExerciseComponent, }
+      //alias example
+      { path: 'demo', canActivate: [CmsPageGuard], component: PageLayoutComponent, data: { pageLabel: '/faq'}},
     ]),
     ConfigModule.withConfig({
       routing: {
